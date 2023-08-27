0d3a694b0f98ef42a6839ca8adada89887280326
Anujsi (Ext.Coforge) <anuj_singh@outlook.in>
2023-08-27 13:08:10 +0530

    fix::app

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,27 +1,31 @@
-export const autosuggest = async (text: string) => {
-    const response = await fetch(
-        `http://localhost:3002/search?text=${text}`
-    );
+const API_URL = "https://unl-be.vercel.app";
 
-    const data = await response.json();
-    return data;
+export const autosuggest = async (text: string) => {
+  const response = await fetch(`${API_URL}/search?text=${text}`);
+
+  const data = await response.json();
+  return data;
 };
 
-export const distance_matrix = async (latitudes: number[], longitudes: number[], unit: string = 'imperial') => {
-    const requestBody = {
-        latitudes,
-        longitudes,
-        unit
-    };
+export const distance_matrix = async (
+  latitudes: number[],
+  longitudes: number[],
+  unit: string = "imperial"
+) => {
+  const requestBody = {
+    latitudes,
+    longitudes,
+    unit,
+  };
 
-    const response = await fetch('http://localhost:3002/distance-matrix', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    })
+  const response = await fetch(`${API_URL}/distance-matrix`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
 
-    const data = await response.json();
-    return data;
-}
+  const data = await response.json();
+  return data;
+};
